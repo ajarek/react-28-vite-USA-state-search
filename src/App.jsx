b15e2af6f5fd8bec0a090583d06f27e3d95d86d7
Ajarek <ajarek@poczta.onet.pl>
2022-10-08 01:15:47 +0200

    added showState

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,81 @@ import { useState } from 'react'
 import Header from './components/Header'
 import { useFetch } from './components/useFetch'
 
-function App() {
+export function App() {
   const [valueSearch, setValueSearch] = useState('')
-
+  const [singleState, setSingleState] = useState('')
   const { data, error } = useFetch('src/assets/states.json')
 
   const InputSearch = (e) => {
     setValueSearch(e.target.value)
   }
+  const showState = (e) => {
+    // eslint-disable-next-line no-return-assign, no-param-reassign
+    const getDataId = data.find((dat) => dat.admission_number === Number(e.target.id))
+    setSingleState((singleState) => getDataId)
+    console.log(singleState)
+  }
+
   return (
     <div className={'App'}>
-      <Header
-        onChange={InputSearch}
-        value={valueSearch}
-      />
-      <div>
-        {data && data.map((el) => (
-          <p
-            key={el.admission_number}
+
+      {singleState
+        ? (
+          <div
+            className={'wrapper-state'}
           >
-            {el.state}
+            <div className={'header-state'}>
+              <h1>{singleState.state}</h1>
+              <div className={'seal'}><img src={singleState.state_seal_url} alt={'seal'} /></div>
+            </div>
+            <div className={'img'}>
+              <img src={singleState.skyline_background_url} alt={'skyline'} />
+              <img src={singleState.map_image_url} alt={'map'} />
+            </div>
+            <div className={'info'}>
+              <p>
+                Capital city:
+                {' '}
+                {singleState.capital_city}
+              </p>
+              <p>
+                Population:
+                {' '}
+                {singleState.population}
+              </p>
+              <p>
+                Admission date:
+                {' '}
+                {singleState.admission_date}
+              </p>
+            </div>
+          </div>
+        )
+        : (
+          <>
+            <Header
+              onChange={InputSearch}
+              value={valueSearch}
+            />
 
-          </p>
-        ))}
+            <div className={'wrapper'}>
+              {data && data.map((el) => (
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+                <div
+                  className={'item'}
+                  key={el.admission_number}
+                  onClick={showState}
+                >
+                  <img src={el.landscape_background_url} alt={'landscape'} id={el.admission_number} />
+                  <p className={'name-state'}>{el.state}</p>
 
-      </div>
+                </div>
+              ))}
+
+            </div>
+          </>
+        )}
+
     </div>
   )
 }
